2d643fc1b6e22cdb55bd937a152afa741dbf2ae8
marcioeugenio <129126182+marcioeugenio@users.noreply.github.com>
2025-06-10 08:48:03 -0300

    layout.tsx

---
+++ src/app/src/app/layout.tsx
@@ -0,0 +1,8 @@
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="pt-br">
+      <head />
+      <body>{children}</body>
+    </html>
+  );
+}
